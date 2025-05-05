825c99a856d445a99f34e8beff9af557be8b24eb
Ryan Arora <arora.ryan@gmail.com>
2025-05-04 21:07:40 -0400

    add minio url to remotepatterns

--- next.config.js
+++ next.config.js
@@ -2,18 +2,13 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
+import { env } from "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
   devIndicators: false,
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
-    ],
+    remotePatterns: [new URL(env.NEXT_PUBLIC_MINIO_URL)],
   },
   experimental: {
     ppr: true,
